d67f1d485ab702765ffc328136863b97bfd1f727
Michał Lach <kontakt@michal-lach.pl>
2016-05-22 12:41:02 +0200

    fixed map component

--- src/js/components/GoogleMap.js
+++ src/js/components/GoogleMap.js
@@ -2,20 +2,22 @@ import React from 'react'
 import { GoogleMapLoader, GoogleMap} from "react-google-maps";
 
 export default (props) => {
+  console.log(props)
   return (
     <section style={{height: "100%"}}>
       <GoogleMapLoader
         containerElement={
           <div
             style={{
-              height: "100%",
+              height: "100px",
+              width: "100px"
             }}
           />
         }
         googleMapElement={
           <GoogleMap
-            defaultZoom={3}
-            defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+            defaultZoom={10}
+            defaultCenter={{ lat: props.lat, lng: props.lon }}
             onClick={props.onMapClick}
           >
           </GoogleMap>
